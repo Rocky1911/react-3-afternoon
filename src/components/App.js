b3f1ab3ed00b793e0d95a370f1e90af7fa40ddbf
Rockydonthurtem <charrisq@hotmail.com>
2018-08-14 15:24:45 -0500

    react3

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "./Post/Post";
+import "./App.css";
 
-import './App.css';
-
-import Header from './Header/Header';
-import Compose from './Compose/Compose';
+import Header from "./Header/Header";
+import Compose from "./Compose/Compose";
 
 class App extends Component {
   constructor() {
@@ -13,25 +14,43 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`).then(results => {
+      console.log(results);
+      this.setState({ posts: results.data });
+    });
   }
 
-  updatePost() {
-  
+  //https://......./api?id=${id} req.query.id
+  //https://......./api/${id} req.params.id
+  //
+  updatePost(id, text) {
+    axios
+      .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, { text })
+      .then(results => {
+        this.setState({ posts: results.data });
+      });
   }
 
-  deletePost() {
-
+  deletePost(id) {
+    axios
+      .delete(`https://practiceapi.devmountain.com/api/posts/?id=${id}`)
+      .then(results => {
+        console.log(results);
+        this.setState({ posts: results.data });
+      });
   }
-
-  createPost() {
-
+  createPost(text) {
+    axios
+      .post(`https://practiceapi.devmountain.com/api/posts`, { text })
+      .then(results => {
+        this.setState({ post: results.data });
+      });
   }
 
   render() {
@@ -42,9 +61,17 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
-          <Compose />
-          
+          <Compose createPostFn={this.createPost} />
+          {posts.map(post => (
+            <Post
+              key={post.id}
+              text={post.text}
+              date={post.date}
+              updatePostFn={this.updatePost}
+              id={post.id}
+              deletePostFn={this.deletePost}
+            />
+          ))}
         </section>
       </div>
     );
